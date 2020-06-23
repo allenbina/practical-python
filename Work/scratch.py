013b4038443696f2db07a94132394e6f5f82ccbb
allen bina <allenbina@gmail.com>
2020-06-22 21:13:21 -0700

    added python launch file

--- Work/scratch.py
+++ Work/scratch.py
@@ -1,4 +1,11 @@
-f = open('Data/portfolio.csv', 'rt')
-data = f.read()
-print(data)
-f.close()
+# f = open('Data/portfolio.csv', 'rt')
+# data = f.read()
+# print(data)
+# f.close()
+
+
+
+import gzip
+with gzip.open('Data/portfolio.csv.gz', 'rt') as f:
+    for line in f:
+        print(line, end='')
